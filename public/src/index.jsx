eb59d8d2ccf4189ca7328f507ed9dea664df1d73
VanshGaur06 <vanshgaur1106@gmail.com>
2025-09-15 19:20:07 +0530

    Create index.jsx

---
+++ public/src/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./FarmQuest_App";
+import "./styles.css"; // Tailwind or your custom styles
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
